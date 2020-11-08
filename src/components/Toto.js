bd908a84b5a057b8c98e5fd338bfffa56330ea06
Habibi Ayoub <habibiayoub87@gmail.com>
2020-11-08 20:50:39 +0100

    Ordre -> Reponse

---
+++ src/components/Toto.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Toto = props => {
+
+    const btnReponseToto = props.leState.messageMaman !== null ? (<button onClick={props.reponseToto}>Réponse</button>) : (<button disabled>Réponse</button>);
+
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            {btnReponseToto}
+            <p>{props.leState.messageToto}</p>
+            <hr />
+        </div>
+    )
+}
+
+export default Toto;
